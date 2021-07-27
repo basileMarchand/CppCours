c8911eac2eebd1aef2d8ad02f6f47f6821f72a63
bmarchand <basile.marchand@gmail.com>
2021-07-27 15:11:22 +0200

    Initial commit

---
+++ src/CppBase/fonction.markdown
@@ -0,0 +1,5 @@
+# Faire des fonctions 
+
+## L'intéret de cloisoner et organiser son code 
+
+\snippet src/CppBase/function_intro.cpp print_coucou
---
+++ src/CppBase/function_intro.cpp
@@ -0,0 +1,26 @@
+
+/**
+ * @file function_intro.cpp
+ * @author Basile Marchand (basile.marchand@mines-paristech.fr)
+ * @brief 
+ * @version 0.1
+ * @date 2021-07-26
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
+#include <iostream>
+
+
+/// [print_coucou]
+void print_coucou(){
+    std::cout << "coucou" << std::endl;
+}
+
+/// [print_coucou]
+
+int main(){
+    print_coucou();
+}
